d2cf9696471fd0f2c00c99221fdc5067b4082c18
toufik <toufikbeycgn@gmail.com>
2022-03-07 17:30:58 +0100

    second challenge

---
+++ reveresInPlace.js
@@ -0,0 +1,17 @@
+function reverse(arrayOfChars) {
+
+    let leftIndex = 0;
+    let rightIndex = arrayOfChars.length - 1;
+  
+    while (leftIndex < rightIndex) {
+  
+      // Swap characters
+      const temp = arrayOfChars[leftIndex];
+      arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+      arrayOfChars[rightIndex] = temp;
+  
+      // Move towards middle
+      leftIndex++;
+      rightIndex--;
+    }
+  }
