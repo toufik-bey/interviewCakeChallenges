81fa8cbd192a11e1d97a97c400e8a64acad96d65
toufik <toufikbeycgn@gmail.com>
2022-03-08 18:28:06 +0100

    complete the 3er challenge

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -3,4 +3,34 @@ function reverseWords(message) {
     // Decode the message by reversing the words
    // the function to reverse a word 
    
-  
+   function reverseWord(array,leftIndex,rightIndex){
+     let x =''; 
+     while(leftIndex<rightIndex){
+       x = array[leftIndex]; 
+       array[leftIndex]=array[rightIndex]; 
+       array[rightIndex]=x; 
+       leftIndex ++; 
+       rightIndex--; 
+     }
+   }; 
+   
+   // reverse the array 
+    reverseWord(message,0,message.length-1); 
+    let current = 0; 
+    for(let i = 0; i<=message.length; i++ ){
+      if( message[i]==' '|| i ===message.length){
+        reverseWord(message,current,i-1); 
+        current = i+1;
+      }
+    }
+   
+   
+   return message 
+   
+   
+     
+     
+     
+     // The easy way : return message.join('').split(' ').reverse(); 
+   
+   }
